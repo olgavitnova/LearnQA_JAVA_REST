e5376c8697c8b79c32a621451ac9954eb410e4ea
Olga Vitnova <olgavintova123@gmail.ru>
2024-03-09 13:51:06 +0300

    Hello World Api Test

---
+++ src/test/java/ApiTest.java
@@ -0,0 +1,15 @@
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Test;
+
+public class ApiTest {
+
+    @Test
+
+    public void TestHelloWorld(){
+        Response response = RestAssured
+                .get("https://playground.learnqa.ru/api/get_text")
+                .andReturn();
+        response.prettyPrint();
+    }
+}
