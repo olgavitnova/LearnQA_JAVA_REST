c76101aa1f6b2692149a001e77e67ddf83450d62
Olga Vitnova <olgavintova123@gmail.ru>
2024-03-14 17:36:04 +0300

    Parsing JSON Ex5 changes

--- src/test/java/JsonParsing.java
+++ src/test/java/JsonParsing.java
@@ -1,13 +1,6 @@
 import io.restassured.RestAssured;
-import io.restassured.http.ContentType;
-import io.restassured.parsing.Parser;
 import io.restassured.path.json.JsonPath;
-import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
-
-import java.util.List;
-
-import static io.restassured.RestAssured.given;
 
 public class JsonParsing {
     @Test
