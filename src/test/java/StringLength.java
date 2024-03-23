c60198075b15a1dcf9a3bbc5e4e2791f66d52059
Olga Vitnova <olgavintova123@gmail.ru>
2024-03-23 14:11:24 +0300

    Ex10: Тест на короткую фразу

---
+++ src/test/java/StringLength.java
@@ -0,0 +1,15 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class StringLength {
+        @Test
+        public void testLength(){
+
+        String str= "Hello, world test!";
+        int length= str.length();
+
+        assertTrue(length>=15,"Условия по длинне переменной не соблюдены");
+        System.out.println(length);
+    }
+}
